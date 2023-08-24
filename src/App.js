d5f1ba2635a015ef741bcf59820d1fee75128637
Ayann12 <tantowi818@gmail.com>
2023-08-24 21:24:17 +0700

    coba lagi

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className='App-Header'>
+          <nav>Nesting</nav>
+        </header>
+        <section>
+          <h1>Halo guys ini mencoba</h1>
+        </section>
       </div>
     );
   }
